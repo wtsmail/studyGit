2015f6b85304192b2afc21e55f0cf6c40f1fab24
tao.wang.sh2 <tao.wang.sh2@dianping.com>
2017-03-20 11:27:23 +0800

    fix

---
+++ src/com/test.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @Author : wangtao
+ * @Data : 2017/3/16.
+ * @Purpose:
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("2016-10-18 15:29:38.493".compareTo("2016-09-25 08:52:03.07"));
+    }
+}
